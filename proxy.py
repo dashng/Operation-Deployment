48bedee793d62e645bf17ad353713876bc1bae52
DAI <dsscnt@163.com>
2017-05-12 23:48:46 +0800

    Create proxy.py
    pass instance to class then return this instance directly

---
+++ proxy.py
@@ -0,0 +1,27 @@
+class Test():
+	def __init__(self):
+		pass
+
+	def run(self):
+		print '======'
+
+	def walk(self):
+		print 'walk...'
+
+t = Test()
+
+
+
+class Proxy(object):
+    def __new__(cls, t):
+    	# print t, cls
+        # return super(Proxy, cls).__new__(cls)
+        return t
+
+    def __init__(self, t):
+        print "INIT"
+
+a = Proxy(t)
+
+a.run()
+a.walk()
